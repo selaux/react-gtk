978bbf0040258e6f2d0db85fb690e349e7f9552a
Stefan Lau <github@stefanlau.com>
2017-09-29 10:48:16 +0200

    Implement controlled input test for toggleables

--- test/functional/apps/inputs.js
+++ test/functional/apps/inputs.js
@@ -4,22 +4,38 @@ const h = React.createElement;
 
 const InputsApp = React.createClass({
     getInitialState() {
-        return { disableEvent: false, value: 0, increment: 1 };
+        return { toggleButtonActive: false, switchActive: false };
+    },
+
+    onToggleButtonActive(btn) {
+        this.setState({ toggleButtonActive: btn.get_active() });
+    },
+
+    setToggleButtonActive() {
+        this.setState({ toggleButtonActive: true })
+    },
+
+    onSwitchSwitched(sw, active) {
+        this.setState({ switchActive: active });
+    },
+
+    setSwitchActive() {
+        this.setState({ switchActive: true })
     },
 
     render() {
-        return h('GtkWindow', { title: 'react-gtk events test', defaultWidth: 200, defaultHeight: 100 },
+        return h('GtkWindow', { title: 'react-gtk inputs test', defaultWidth: 200, defaultHeight: 100 },
             h('GtkVBox', {}, [
                 h('GtkHBox', { key: 0 }, [
-                    h('GtkToggleButton', { label: 'Toggle me' }),
-                    h('GtkButton', { label: 'Set Value' })
+                    h('GtkToggleButton', { label: 'Toggle me', onToggled: this.onToggleButtonActive, active: this.state.toggleButtonActive }),
+                    h('GtkButton', { label: 'Set To Active', onClicked: this.setToggleButtonActive })
                 ]),
                 h('GtkHBox', { key: 1 }, [
-                    h('GtkSpinButton', {}),
-                    h('GtkButton', { label: 'Set Value' })
+                    h('GtkSwitch', { active: this.state.switchActive, onStateSet: this.onSwitchSwitched }),
+                    h('GtkButton', { label: 'Set To Active', onClicked: this.setSwitchActive })
                 ]),
                 h('GtkHBox', { key: 2 }, [
-                    h('GtkSwitch', {}),
+                    h('GtkSpinButton', {}),
                     h('GtkButton', { label: 'Set Value' })
                 ]),
                 h('GtkHBox', { key: 3 }, [
